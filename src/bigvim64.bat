814e7b80e9706989f5300a1bc1397d26594a606c
Per-Jonny Käck <pjkack@gmail.com>
2025-11-01 19:58:39 +0100

    Update path to vcvarsall in bigvim.bat files.

--- src/bigvim64.bat
+++ src/bigvim64.bat
@@ -1,5 +1,5 @@
 :: command to build big Vim 64 bit with/without Python for Windows 7
-:: call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:: call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set VIM_ADD_FEATURES=FEATURES=BIG GUI=yes DIRECTX=yes COLOR_EMOJI=yes TERMINAL=yes
 :: PYTHON=C:\python27 DYNAMIC_PYTHON=yes PYTHON_VER=27
 set VIM_REMOVE_FEATURES=CSCOPE=no NETBEANS=no XPM=no
